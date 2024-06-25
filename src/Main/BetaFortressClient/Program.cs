fae5b595f6f8005c9d932f88246208359d259770
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2024-06-25 12:07:45 +0800

    rah

--- src/Main/BetaFortressClient/Program.cs
+++ src/Main/BetaFortressClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using BetaFortressTeam.BetaFortressClient.Util;
 using System.Diagnostics;
+using Sentry;
+using Sentry.Profiling;
 
 namespace BetaFortressTeam.BetaFortressClient
 {
@@ -9,6 +11,34 @@ namespace BetaFortressTeam.BetaFortressClient
         static void Main()
         {
             Console.WriteLine("[BFCLIENT] Starting up the program...");
+            // sentry must be the intialized first!!!
+            // Init the Sentry SDK
+            SentrySdk.Init(o =>
+            {
+                // Tells which project in Sentry to send events to:
+                o.Dsn = "dsn-link";
+                // When configuring for the first time, to see what the SDK is doing:
+                o.Debug = true; // dont set to true pls -medicineaddict
+                // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production.
+                o.TracesSampleRate = 1.0;
+                // Sample rate for profiling, applied on top of othe TracesSampleRate,
+                // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+                // We recommend adjusting this value in production.
+                o.ProfilesSampleRate = 1.0;
+
+                o.AutoSessionTracking = true;
+
+                // Requires NuGet package: Sentry.Profiling
+                // Note: By default, the profiler is initialized asynchronously. This can
+                // be tuned by passing a desired initialization timeout to the constructor.
+                o.AddIntegration(new ProfilingIntegration(
+                    // During startup, wait up to 500ms to profile the app startup code.
+                    // This could make launching the app a bit slower so comment it out if you
+                    // prefer profiling to start asynchronously
+                    TimeSpan.FromMilliseconds(500)
+                ));
+            });
 
             try
             {
@@ -21,7 +51,11 @@ namespace BetaFortressTeam.BetaFortressClient
             }
             catch (Exception ex) 
             {
-                Gui.Message(ex.ToString(), 0);
+                SentrySdk.CaptureException(ex);
+                Gui.Message(ex.ToString() + "\n", 0);
+                Gui.MessageWaitForKey("We're sorry that you've experienced this problem.\n" +
+                    "Don't worry! This issue reported and our team is working on it!\n" +
+                    "Press any key to exit...");
             }
         }
 
@@ -61,6 +95,44 @@ namespace BetaFortressTeam.BetaFortressClient
                     }
                     break;
                 case "2":
+                    #if _WINDOWS
+                    if(ModManager.IsModInstalled)
+                    {
+                        if(File.Exists(ModManager.GetModPath + "/cfg/server.cfg"))
+                        {
+                            if(Gui.MessageYesNo("The server configuration file exists. Do you want to edit it now?"))
+                            {
+                                Process p = new Process();
+                                p.StartInfo.FileName = "C:\\windows\\notepad.exe";
+                                p.StartInfo.Arguments = ModManager.GetModPath + "/cfg/server.cfg";
+                                p.Start();
+                                p.WaitForExit();
+                            }
+                        }
+                        else
+                        {
+                            File.CreateText(ModManager.GetModPath + "/cfg/server.cfg");
+
+                            if(Gui.MessageYesNo("Created the server configuration file. Do you want to edit it now?"))
+                            {
+                                Process p = new Process();
+                                p.StartInfo.FileName = "C:\\windows\\notepad.exe";
+                                p.StartInfo.Arguments = ModManager.GetModPath + "/cfg/server.cfg";
+                                p.Start();
+                                p.WaitForExit();
+                            }
+                        }
+
+                        if(Gui.MessageYesNo("Do you want to go back to the menu?"))
+                        {
+                            RunInteractive();
+                        }
+                        else
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                    #else
                     string dir2 = Gui.MessageInput("Please enter where your Steam's sourcemods directory:");
                     if(Directory.Exists(dir2) && Directory.Exists(dir2 + "/bf"))
                     {
@@ -68,10 +140,9 @@ namespace BetaFortressTeam.BetaFortressClient
                         
                         if(File.Exists(dir2 + "/cfg/server.cfg"))
                         {
-                            if(Gui.MessageYesNo("Created the server configuration file. Do you want to edit it now?"))
+                            if(Gui.MessageYesNo("The server configuration file exists. Do you want to edit it now?"))
                             {
                                 Process p = new Process();
-                                p.StartInfo = new ProcessStartInfo();
                                 #if _WINDOWS
                                 p.StartInfo.FileName = "C:\\windows\\notepad.exe";
                                 #else
@@ -79,7 +150,7 @@ namespace BetaFortressTeam.BetaFortressClient
                                 #endif
                                 p.StartInfo.Arguments = dir2 + "/cfg/server.cfg";
                                 p.Start();
-                                Task.Run(async() => await p.WaitForExitAsync());
+                                p.WaitForExit();
                             }
                         }
                         else
@@ -97,7 +168,7 @@ namespace BetaFortressTeam.BetaFortressClient
                                 #endif
                                 p.StartInfo.Arguments = dir2 + "/cfg/server.cfg";
                                 p.Start();
-                                Task.Run(async() => await p.WaitForExitAsync());
+                                p.WaitForExit();
                             }
                         }
                     }
@@ -105,6 +176,7 @@ namespace BetaFortressTeam.BetaFortressClient
                     {
                         Gui.Message("Beta Fortress is not installed.\n", 0);
                     }
+                    #endif
 
                     if(Gui.MessageYesNo("Do you want to go back to the menu?"))
                     {
